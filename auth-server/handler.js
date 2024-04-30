7efcb00158427cd59acd9700ea149535f465680f
devmcdonough <devmcdonough@gmail.com>
2024-04-30 16:16:23 -0400

    Added function to handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,5 +1,37 @@
 'use strict';
 
+const { google } = require("googleapis");
+const { jsx } = require("react/jsx-runtime");
+const calendar = google.calendar("v3");
+const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const redirect_uris = [ "https://devmcdonough.github.io/meet"];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  redirect_uris[0]
+);
+
+module.exports.getAuthURL = async () => {
+  /* Scopes array passed to the scope option (I don't know what this means but ok) */
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      authUrl,
+    }),
+  };
+};
+
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
